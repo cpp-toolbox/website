90f9046970523c96eb3d72ff66fe6a9470534964
ccn <ccn@cuppajoeman.com>
2025-09-26 17:50:51 -0400

    feat: add in code for toolbox-engine

---
+++ code/src/sound/sound_types/sound_types.hpp
@@ -0,0 +1,9 @@
+#ifndef SOUND_TYPES_HPP
+#define SOUND_TYPES_HPP
+// Enum representing different sound types
+enum class SoundType {
+    SOUND_1,
+    SOUND_2,
+    SOUND_3,
+};
+#endif // SOUND_TYPES_HPP
